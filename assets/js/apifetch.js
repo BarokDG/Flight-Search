356494f691a25ec87686ff3efa45e04c77cce7c2
BarokDG <it.barok.dagim@gmail.com>
2021-02-27 13:56:52 +0300

    Data display on UI done

--- assets/js/apifetch.js
+++ assets/js/apifetch.js
@@ -1,13 +1,23 @@
 var form = document.getElementById('form')
-var result = document.getElementById('result')
+var result = document.getElementById('loadData')
 var search = document.querySelector("#searchBtn")
 // form.addEventListener('submit',getData)
 var jsonData
 
+async function loadData(country,currency,origin,destination,obd){
 
+    let result =  await fetch(`https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browseroutes/v1.0/${country}/${currency}/en-US/${origin}/${destination}/${obd}`, {
+        "method": "GET",
+        "headers": {
+            "content-type" : "application/json",
+            "x-rapidapi-key": "d064331107msh09cdb17875f4ce2p120f58jsnf9fbfec77964",
+            "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
+        }
+    })
 
-
-
+    let data = await result.json()
+    return data
+}
 
 search.addEventListener("click",searchFlights )
 function searchFlights(){
@@ -16,39 +26,49 @@ function searchFlights(){
     var origin  = form.origin.value
     var destination = form.destination.value
     var obd = form.obd.value
-
-    var data = loadData(country, currency, origin, destination, obd)
-    displayResults(data)
+    
+    displayResults(country, currency, origin, destination, obd)
     
 }
 
-function displayResults(data){
-    data.then((flights) => { jsonData = flights }) 
-    // jsonData contains returned json data
-}
-
-
-
-
-async function loadData(country,currency,origin,destination,obd){
-
-    let result =  await fetch(`https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browseroutes/v1.0/${country}/${currency}/en-US/${origin}/${destination}/${obd}`, {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-key": "d064331107msh09cdb17875f4ce2p120f58jsnf9fbfec77964",
-            "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
+function displayResults(country, currency, origin, destination, obd){
+    loadData(country, currency, origin, destination, obd).then( function(flights) { 
+        jsonData = flights 
+        console.log(flights);
+        
+        // jsonData contains returned json data
+        let output = ''
+        let table = []
+        let carriers = {}
+        let rowCount = 0
+        for(let item in flights["Carriers"]){
+            carriers[(flights["Carriers"][item]["CarrierId"])] = (flights["Carriers"][item]["Name"])
+            // table[rowCount].push(flights["Quotes"][item]["MinPrice"])
         }
+        
+        for(let item in flights["Quotes"]){
+            table.push([])
+            table[rowCount].push(flights["Quotes"][item]["OutboundLeg"]["CarrierIds"][0])
+            table[rowCount].push(flights["Quotes"][item]["MinPrice"])
+            table[rowCount].push(flights["Quotes"][item]["OutboundLeg"]["DepartureDate"])
+            rowCount += 1
+        }
+
+        console.log(table);
+
+        for(let item in table){
+            output += `
+                    <tr>
+                        <td>${carriers[table[item][0]]}</td>
+                        <td>${table[item][1]}${form.currency.value}</td>
+                        <td>${table[item][2]}</td>
+                        <td>---</td>
+                    </tr>
+                    `
+        }
+        result.innerHTML = output;
     })
-    .then(response => {
-        return response
-    })
-    .catch(err => {
-        console.error(err);
+    .catch(function(err) {
+        console.log(err);
     });
-
-    let data =result.json()
-    return data
 }
-
-
-
--- assets/js/history.js
+++ assets/js/history.js
@@ -2,29 +2,28 @@ const input = document.querySelector('#searchKey')
 const btn = document.querySelector('button')
 const historyList = document.querySelector('#history')
 
-
-onload = displayHistory()
+// onload = displayHistory()
 
 // btn.addEventListener('click', searchFunction)
-function searchFunction(){
+function searchFunction() {
     saveSearchRecord(input.value, Date())
 }
 
-function saveSearchRecord(searchKey, searchDate){
+function saveSearchRecord(searchKey, searchDate) {
     let shJSON
     if (localStorage.getItem('searchHistory') == null)
         localStorage.setItem('searchHistory', "{}")
-    
+
     shJSON = localStorage.getItem('searchHistory')
-    let searchHistory =  JSON.parse(shJSON)
+    let searchHistory = JSON.parse(shJSON)
     searchHistory[searchKey] = searchDate
-    shJSON = JSON.stringify(searchHistory)    
+    shJSON = JSON.stringify(searchHistory)
     localStorage.setItem('searchHistory', shJSON)
     displayHistory()
 }
 
-function displayHistory(){
-    while(historyList.childElementCount != 0){
+function displayHistory() {
+    while (historyList.childElementCount != 0) {
         historyList.removeChild(historyList.firstChild)
     }
 
@@ -35,17 +34,17 @@ function displayHistory(){
     let searchHistory = JSON.parse(shJSON)
     let searchHistoryList = []
     console.log(searchHistory);
-    
-    for (var key in searchHistory){
+
+    for (var key in searchHistory) {
         let a = document.createElement('a')
         a.className = 'list-group-item list-group-item-action'
         a.textContent = `${key} \t\t\t\t${searchHistory[key]}`
         searchHistoryList.push(a)
     }
 
-    for(const element of searchHistoryList.reverse())
+    for (const element of searchHistoryList.reverse())
         historyList.appendChild(element)
 }
 
-$('table').DataTable({
-});
+// $('table').DataTable({
+// });
